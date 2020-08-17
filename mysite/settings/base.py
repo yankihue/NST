62d328ed1df9dab32875c879617472820963e6da
thedoctorfunk <drfunkthedoc@gmail.com>
2020-08-17 17:52:07 +0300

    fix whitenoise

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
 
-    'django.middleware.security.SecurityMiddleware',
+    # 'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -133,19 +133,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = [
+# STATICFILES_DIRS = [
 
 
-    os.path.join(BASE_DIR, "static")
+#     os.path.join(BASE_DIR, "static")
 
 
-]
+# ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,10 +8,16 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+from pathlib import Path
 
+from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings.prod')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, os.path.join(BASE_DIR, 'static'))
+application.add_files(os.path.join(BASE_DIR, 'static'))
