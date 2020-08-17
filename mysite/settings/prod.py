99442eff41612f6880da387fceab30e8c6dc92af
thedoctorfunk <drfunkthedoc@gmail.com>
2020-08-17 17:28:07 +0300

    add ssl header

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -9,6 +9,9 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 ALLOWED_HOSTS = ['nisanyantube.herokuapp.com', '*', 'localhost', '127.0.0.1', '[::1]']
 
 
